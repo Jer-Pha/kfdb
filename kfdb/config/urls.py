50111a6c5e6c75ca2a420b097ffcce34e8f2e355
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-06-11 23:13:18 -0700

    Combine ViewSets to single router

--- kfdb/config/urls.py
+++ kfdb/config/urls.py
@@ -1,13 +1,29 @@
 """URL configuration for kfdb project."""
 
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from apps.channels.views import ChannelViewSet
+from apps.episodes.views import EpisodeViewSet
+from apps.hosts.views import HostViewSet
+from apps.shows.views import ShowViewSet
+
+router = DefaultRouter()
+router.register("channels", ChannelViewSet, basename="channels")
+router.register("episodes", EpisodeViewSet, basename="episodes")
+router.register("hosts", HostViewSet, basename="hosts")
+router.register("shows", ShowViewSet, basename="shows")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
-    path("channels/", include("apps.channels.urls")),
-    path("episodes/", include("apps.episodes.urls")),
-    path("hosts/", include("apps.hosts.urls")),
-    path("shows/", include("apps.shows.urls")),
+    path("api/", include(router.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
