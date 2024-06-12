4504ab92fd9e9b8d89ab9484590fbcf8e5d2ed50
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-06-11 23:16:07 -0700

    Initial commit

---
+++ kfdb/apps/hosts/serializers.py
@@ -0,0 +1,28 @@
+from copy import deepcopy
+
+from rest_framework import serializers
+
+from .models import Host
+
+
+class HostSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Host
+        fields = [
+            "name",
+            "nicknames",
+            "kf_crew",
+            "part_timer",
+            "socials",
+            "birthday",
+            "blurb",
+        ]
+
+    def to_representation(self, instance):
+        obj = super().to_representation(instance)
+        non_null = deepcopy(obj)
+        for key in obj.keys():
+            if obj[key] is None:
+                non_null.pop(key)
+        return non_null
