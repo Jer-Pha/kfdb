4c27500d0afe49ce671ac02db485485f2fbc8267
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-06-13 20:53:28 -0700

    Add temporary view

--- kfdb/config/urls.py
+++ kfdb/config/urls.py
@@ -6,7 +6,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from apps.channels.views import ChannelViewSet
-from apps.episodes.views import EpisodeViewSet
+from apps.episodes.views import EpisodeViewSet, upload_view  # Temporary
 from apps.hosts.views import HostViewSet
 from apps.shows.views import ShowViewSet
 
@@ -21,6 +21,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/", include(router.urls)),
+    path("temp/upload/", upload_view),  # Temporary
 ]
 
 if settings.DEBUG:  # pragma: no cover
