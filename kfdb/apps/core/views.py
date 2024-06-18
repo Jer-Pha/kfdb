66d9a49bbd3dcdc7d8cb56f950d703e49b1c2abe
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-06-18 01:48:58 -0700

    Update views.py

--- kfdb/apps/core/views.py
+++ kfdb/apps/core/views.py
@@ -1,23 +1,44 @@
 from django.db.models import Count, Q
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.views.decorators.http import require_GET
 
 from apps.hosts.models import Host
 from apps.videos.models import Video
 
 
+def homepage(request):
+    context = {}
+
+    return render(request, "core/index.html", context)
+
+
 @require_GET
 def update_index_stats(request):
-    c = Host.objects.only("pk", "kf_crew", "part_timer").aggregate(
+    count = Host.objects.only("pk", "kf_crew", "part_timer").aggregate(
         crew=Count("pk", filter=Q(kf_crew=True, part_timer=False)),
         pt=Count("pk", filter=Q(kf_crew=False, part_timer=True)),
         guest=Count("pk", filter=Q(kf_crew=False, part_timer=False)),
     )
-    video_count = Video.objects.all().count()
+    count["video"] = Video.objects.all().count()
 
-    return HttpResponse(
-        f"<span>{c['crew']}</span>"
-        f'<span hx-swap-oob="#part-timer-count">{c['pt']}</span>'
-        f'<span hx-swap-oob="#guest-count">{c['guest']}</span>'
-        f'<span hx-swap-oob="#video-count">{video_count}</span>'
+    context = {
+        "count": count,
+    }
+
+    return render(request, "core/partials/update-index-stats.html", context)
+
+
+@require_GET
+def update_index_stats(request):
+    count = Host.objects.only("pk", "kf_crew", "part_timer").aggregate(
+        crew=Count("pk", filter=Q(kf_crew=True, part_timer=False)),
+        pt=Count("pk", filter=Q(kf_crew=False, part_timer=True)),
+        guest=Count("pk", filter=Q(kf_crew=False, part_timer=False)),
     )
+    count["video"] = Video.objects.all().count()
+
+    context = {
+        "count": count,
+    }
+
+    return render(request, "core/partials/update-index-stats.html", context)
