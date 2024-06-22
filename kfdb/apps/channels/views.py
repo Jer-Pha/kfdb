7398ca10ee3446e967634d06bc38d2dc6c09b349
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-06-21 23:03:13 -0700

    Start foundation for filter and pagination

--- kfdb/apps/channels/views.py
+++ kfdb/apps/channels/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views.decorators.http import require_GET
 
@@ -16,13 +16,33 @@ def channels_home(request):
 def channel_page(request, channel):
     page = request.GET.get("page", 1)
     search = request.GET.get("search", "")
-    host_filter = request.GET.get("hosts", "").split(",")
-    guest_filter = request.GET.get("guest", "").split(",")
-    show_filter = request.GET.get("shows", "").split(",")
+    filter_crew = request.GET.get("crew", "").split(",")
+    filter_part_timers = request.GET.get("part-timers", "").split(",")
+    filter_guests = request.GET.get("guests", "").split(",")
+    filter_show = request.GET.get("shows", "").split(",")
+    results_per_page = request.GET.get("per_page", 25)
+
+    channel = Channel.objects.values().get(slug=channel)
+
+    build_filter = {
+        "shows_active": list(
+            Video.objects.select_related("show")
+            .filter(channel=channel["id"], show__active=True)
+            .distinct()
+            .values_list("show__slug", "show__name")
+            .order_by("show__name")
+        ),
+        "shows_inactive": list(
+            Video.objects.select_related("show")
+            .filter(channel=channel["id"], show__active=False)
+            .distinct()
+            .values_list("show__slug", "show__name")
+            .order_by("show__name")
+        ),
+    }
 
     # Standard page load
     if not "HX-Request" in request.META:
-        channel = Channel.objects.values().get(slug=channel)
         videos = (
             Video.objects.select_related("show")
             .only(
@@ -34,16 +54,17 @@ def channel_page(request, channel):
                 "show__image",
             )
             .filter(channel=channel["id"])
-            .order_by("-release_date")[:10]
+            .order_by("-release_date")[:results_per_page]
         )
-
-        print(channel)
 
         context = {
             "channel": channel,
             "videos": videos,
+            "build_filter": build_filter,
         }
 
         return render(request, "channels/channel-page.html", context)
 
     # HTMX request for pagination
+    paginator = Paginator(qs, results_per_page)
+    page_count = paginator.num_pages
