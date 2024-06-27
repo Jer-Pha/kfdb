f5ac52a1a1f7a6235e7e7cd604f7d2b18dda7220
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-06-27 00:37:53 -0700

    Activate individual show pages

--- kfdb/apps/shows/urls.py
+++ kfdb/apps/shows/urls.py
@@ -8,9 +8,9 @@ from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path(
-        "<slug:channel>/shows/",
+        "shows/",
         RedirectView.as_view(permanent=True, pattern_name="channel_page"),
         name="show_home",
     ),
-    path("<slug:channel>/<slug:show>/", show_page, name="show_page"),
+    path("s/<slug:show>/", show_page, name="show_page"),
 ]
--- kfdb/apps/shows/views.py
+++ kfdb/apps/shows/views.py
@@ -1,6 +1,93 @@
-from django.http import HttpResponse
+from django.conf import settings
+from django.core.paginator import Paginator
+from django.db import connection
+from django.db.models import Q
 from django.shortcuts import render
+from django.views.decorators.http import require_GET
+
+from .models import Show
+from apps.videos.models import Video
 
 
-def show_page(request, channel, show):
-    return HttpResponse()
+@require_GET
+def show_page(request, show):
+    page = int(request.GET.get("page", 1))
+    sort = request.GET.get("sort", "-release_date")
+    search = request.GET.get("search", "")
+    filter_channel = request.GET.get("channel", "")
+    filter_guest = request.GET.get("guest", "")
+    filter_producer = request.GET.get("producer", "")
+    filter_part_timer = request.GET.get("part-timer", "")
+    filter_crew = dict(request.GET).get("crew", [])
+    results_per_page = request.GET.get("results", 25)
+
+    show = Show.objects.values().get(slug=show)
+    videos = Video.objects.select_related("channel")
+    filter_params = {"show": show["id"]}
+
+    if filter_channel:
+        filter_params["channel__slug"] = filter_channel
+
+    if filter_producer:
+        filter_params["producer__slug"] = filter_producer
+
+    if search and not settings.DEBUG:
+        videos = videos.filter(
+            Q(blurb__search=search) | Q(title__search=search)
+        )
+    elif search:
+        videos = videos.filter(
+            Q(blurb__icontains=search) | Q(title__icontains=search)
+        )
+
+    if filter_crew:
+        if filter_guest:
+            filter_crew.append(filter_guest)
+        if filter_part_timer:
+            filter_crew.append(filter_part_timer)
+
+        for host in filter_crew:
+            videos = videos.filter(hosts__slug=host)
+    else:
+        if filter_guest:
+            videos = videos.filter(hosts__slug=filter_guest)
+        if filter_part_timer:
+            videos = videos.filter(hosts__slug=filter_part_timer)
+
+    videos = (
+        videos.filter(**filter_params)
+        .only(
+            "title",
+            "video_id",
+            "release_date",
+            "show",
+            "show__name",
+            "show__image",
+            "channel",
+        )
+        .order_by(sort)
+    )
+
+    paginator = Paginator(videos, results_per_page)
+    page_count = paginator.num_pages
+    videos = paginator.get_page(page).object_list
+
+    print(page_count)
+
+    context = {
+        "videos": videos,
+    }
+
+    # Standard page load
+    if "Hx-Boosted" in request.headers or not "Hx-Request" in request.headers:
+
+        context.update(
+            {
+                "show": show,
+                "curr_path": request.path,
+            }
+        )
+
+        return render(request, "shows/show-page.html", context)
+
+    return render(request, "videos/partials/get-video-results.html", context)
