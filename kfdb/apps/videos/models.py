2055c689d1fad928f55e4295beeb2cf4a4a48198
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-06-27 13:43:05 -0700

    Move custom search lookup to models.py

--- kfdb/apps/videos/models.py
+++ kfdb/apps/videos/models.py
@@ -50,6 +50,18 @@ SHOWS_GAMES = [
 ]
 
 
+@models.CharField.register_lookup
+@models.TextField.register_lookup
+class MySqlSearch(models.Lookup):
+    lookup_name = "search"
+
+    def as_mysql(self, compiler, connection):
+        lhs, lhs_params = self.process_lhs(compiler, connection)
+        rhs, rhs_params = self.process_rhs(compiler, connection)
+        params = lhs_params + rhs_params
+        return "MATCH (%s) AGAINST (%s IN BOOLEAN MODE)" % (lhs, rhs), params
+
+
 class Video(models.Model):
     title = models.CharField(
         max_length=255,
