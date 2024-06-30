32246358a733c5c76198eb82abd3c4af9ec1e7b7
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-06-30 02:52:23 -0700

    Add page-search input element

--- kfdb/apps/core/static/js/searchHandler.js
+++ kfdb/apps/core/static/js/searchHandler.js
@@ -1,9 +1,12 @@
-var searchBox = document.getElementById('search-input');
-
-searchBox?.addEventListener('keydown', (e) => {
+// Prevent HTML forms from being submitted by the Enter/Return key.
+// Without this, forms will submit the GET request to a new page instead
+// of using htmx.
+var inputs = document.querySelectorAll('.form-input');
+function stopSubmit(e) {
     if (e.key === 'Enter') {
-        searchBox.blur();
+        e.currentTarget.blur();
         return false;
     }
     return true;
-});
+}
+inputs?.forEach(i => i.addEventListener('keydown', stopSubmit));
