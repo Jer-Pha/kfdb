46dd441c543fe987a068183ca3a40236219bd488
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-07-02 22:22:26 -0700

    Fix pagination number input
    Each new page was dropping the event listener.

--- kfdb/apps/core/static/js/resultsHandler.js
+++ kfdb/apps/core/static/js/resultsHandler.js
@@ -1,5 +1,38 @@
-function fadeOut(bool) {
+// Hiide video detail buttons once htmx request completes
+document.body.addEventListener('htmx:afterRequest', function (e) {
+    if (
+        e.detail.requestConfig.headers['HX-RemoveNode'] && e.detail.successful
+    ) {
+        e.target.setAttribute('hidden', 'true');
+        let parent = e.target.parentNode;
+        if (!e.target.parentNode.querySelector(':scope > :not([hidden])')) {
+            parent.remove();
+        }
+    } else if (
+        e.detail.requestConfig.headers['HX-UpdateList'] && e.detail.successful
+    ) {
+        document.getElementById('page-input')
+            .addEventListener('keydown', stopSubmit);
+    }
+});
+
+// Add fade out animation before htmx swaps new results (w/ fade in)
+function fadeOut() {
     document.querySelectorAll('.results-item')?.forEach(o => {
         o.classList.add('fade-out');
     });
 }
+
+// Prevent HTML forms from being submitted by the Enter/Return key.
+// Without this, forms will submit the GET request to a new page instead
+// of using htmx.
+var inputs = document.querySelectorAll('.form-input');
+function stopSubmit(e) {
+    console.log(e.key);
+    if (e.key === 'Enter') {
+        e.currentTarget.blur();
+        return false;
+    }
+    return true;
+}
+inputs?.forEach(i => i.addEventListener('keydown', stopSubmit));
