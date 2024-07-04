110326283761ecd5e87d41238d931506d30e77b2
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-07-03 21:29:59 -0700

    Add 'xs' viewport breakpoint and replace all 'sm:'

--- kfdb/tailwind.config.js
+++ kfdb/tailwind.config.js
@@ -20,6 +20,20 @@ module.exports = {
     'sm:max-w-8',
   ],
   theme: {
+    screens: {
+      'xs': '432px',
+      // => @media (min-width: 432px) { ... }
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {},
   },
   daisyui: {
