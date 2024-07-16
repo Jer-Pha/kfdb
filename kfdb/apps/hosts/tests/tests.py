d7b8e291baa8a67793da29dff94457332ab05deb
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-07-15 20:05:54 -0700

    Add single host view tests

--- kfdb/apps/hosts/tests/tests.py
+++ kfdb/apps/hosts/tests/tests.py
@@ -1,9 +1,13 @@
+from datetime import datetime
+
 from django.core.files.uploadedfile import SimpleUploadedFile
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
+from django.urls import reverse
 from rest_framework.test import APIRequestFactory
 
 from ..models import Host
 from ..serializers import HostSerializer
+from ..views import HostPageView
 
 # Bytes representing a valid 1-pixel PNG
 ONE_PIXEL_PNG_BYTES = (
@@ -31,6 +35,99 @@ class HostModelTest(TestCase):
     def test_model_str(self):
         """Tests model __str__."""
         self.assertEqual(str(self.host), self.host.name)
+
+
+class HostViewsTest(TestCase):
+    """Tests Host views."""
+
+    def setUp(self):
+        """Sets up test data."""
+        Host.objects.create(
+            name="Test Crew",
+            slug="test-crew",
+            image=SimpleUploadedFile(
+                name="test.png",
+                content=ONE_PIXEL_PNG_BYTES,
+                content_type="image/png",
+            ),
+            image_xs=SimpleUploadedFile(
+                name="test-xs.png",
+                content=ONE_PIXEL_PNG_BYTES,
+                content_type="image/png",
+            ),
+            nicknames=["Test Nickname"],
+            kf_crew=True,
+            part_timer=False,
+            socials=["https://www.twitter.com/test"],
+            birthday=datetime.now().date(),
+            blurb="Test blurb",
+        )
+        Host.objects.create(
+            name="Test Part Timer",
+            slug="test-part-timer",
+            image=SimpleUploadedFile(
+                name="test.png",
+                content=ONE_PIXEL_PNG_BYTES,
+                content_type="image/png",
+            ),
+            image_xs=SimpleUploadedFile(
+                name="test-xs.png",
+                content=ONE_PIXEL_PNG_BYTES,
+                content_type="image/png",
+            ),
+            nicknames=["Test Nickname"],
+            kf_crew=False,
+            part_timer=True,
+            socials=["https://www.twitter.com/test"],
+            birthday=datetime.now().date(),
+            blurb="Test blurb",
+        )
+        Host.objects.create(
+            name="Test Guest",
+            slug="test-guest",
+            image=SimpleUploadedFile(
+                name="test.png",
+                content=ONE_PIXEL_PNG_BYTES,
+                content_type="image/png",
+            ),
+            image_xs=SimpleUploadedFile(
+                name="test-xs.png",
+                content=ONE_PIXEL_PNG_BYTES,
+                content_type="image/png",
+            ),
+            nicknames=["Test Nickname"],
+            kf_crew=False,
+            part_timer=False,
+            socials=["https://www.twitter.com/test"],
+            birthday=datetime.now().date(),
+            blurb="Test blurb",
+        )
+
+    def test_new_page(self):
+        """Tests view when `self.new_page == True`."""
+        request = RequestFactory().get(
+            reverse("host_page", kwargs={"type": "guest", "host": "test-crew"})
+        )
+        view = HostPageView()
+        view.setup(request)
+        view.get(request, host="test-crew")
+        context = view.get_context_data(host="test-crew")
+        self.assertIn("videos", context)
+        self.assertIn("filter_param", context)
+
+    def test_xhr_request(self):
+        """Tests view when `self.new_page == False`."""
+        request = RequestFactory(headers={"Hx-Request": True}).get(
+            reverse(
+                "host_page", kwargs={"type": "guest", "host": "test-guest"}
+            ),
+        )
+        view = HostPageView()
+        view.setup(request)
+        view.get(request, host="test-crew")
+        context = view.get_context_data(host="test-crew")
+        self.assertIn("videos", context)
+        self.assertNotIn("filter_param", context)
 
 
 class HostSerializerTest(TestCase):
