d8592186f4df4cb4f3d200a7c8f29087e83d9617
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-07-16 21:43:24 -0700

    Add coverage exclusion to admin actions

--- kfdb/apps/videos/admin.py
+++ kfdb/apps/videos/admin.py
@@ -17,19 +17,19 @@ class VideoAdmin(admin.ModelAdmin):
     actions = ("channel_prime", "channel_games", "channel_members")
 
     @admin.action(description="Set Channel - KF Prime")
-    def channel_prime(modeladmin, request, queryset):
+    def channel_prime(modeladmin, request, queryset):  # pragma: no cover
         channel = Channel.objects.only("pk").get(slug="prime")
         queryset.update(channel=channel)
         messages.success(request, "Successfully changed to KF Prime!")
 
     @admin.action(description="Set Channel - KF Games")
-    def channel_games(modeladmin, request, queryset):
+    def channel_games(modeladmin, request, queryset):  # pragma: no cover
         channel = Channel.objects.only("pk").get(slug="games")
         queryset.update(channel=channel)
         messages.success(request, "Successfully changed to KF Games!")
 
     @admin.action(description="Set Channel - KF Membership")
-    def channel_members(modeladmin, request, queryset):
+    def channel_members(modeladmin, request, queryset):  # pragma: no cover
         channel = Channel.objects.only("pk").get(slug="members")
         queryset.update(channel=channel)
         messages.success(request, "Successfully changed to KF Membership!")
