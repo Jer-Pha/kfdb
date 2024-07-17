3b645ea1d6b701470febf76dc74e40d674e924a1
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-07-16 17:49:38 -0700

    Add core/views.py tests (filter params)

--- kfdb/apps/core/tests/tests.py
+++ kfdb/apps/core/tests/tests.py
@@ -24,7 +24,7 @@ class CoreViewsTest(TestCase):
         producer = Host.objects.create(name="Test Producer")
         show = Show.objects.create(name="Test Show")
 
-        for i in range(10):
+        for i in range(60):
             video_id = get_random_string(length=11)
             video = Video.objects.create(
                 title=f"Test Video ({video_id})",
@@ -41,20 +41,47 @@ class CoreViewsTest(TestCase):
             if not i % 3 or not i % 5:
                 video.hosts.add(part_timer)
 
-    def test_video_filter(self):
-        """Tests build_filter()."""
-        request = RequestFactory().get(
-            reverse("videos_home")
-            + (
-                "?page=1&sort=release_date&search=test%20video"
-                "&channel=test-channel&show=test-show&guest=test-guest"
-                "&producer=test-producer&part-timer=test-part-timer"
-                "&crew=test-crew&results=100"
+    def test_get_videos(self):
+        """Tests get_videos()."""
+        tests = [
+            {
+                "params": (
+                    "?page=1&sort=title&search=test%20video"
+                    "&channel=test-channel&show=test-show&guest=test-guest"
+                    "&producer=test-producer&part-timer=test-part-timer"
+                    "&crew=test-crew&results=100"
+                ),
+                "page_range": None,
+                "count": 14,
+            },
+            {
+                "params": "?page=1&sort=-title&results=10",
+                "page_range": [1, 2, 3, "...", 6],
+                "count": 10,
+            },
+            {
+                "params": "?page=2&results=25",
+                "page_range": range(1, 4),
+                "count": 25,
+            },
+            {
+                "params": "?page=3&results=10",
+                "page_range": [1, "...", 2, 3, 4, "...", 6],
+                "count": 10,
+            },
+            {
+                "params": "?page=6&results=10",
+                "page_range": [1, "...", 4, 5, 6],
+                "count": 10,
+            },
+        ]
+
+        for test in tests:
+            request = RequestFactory().get(
+                reverse("videos_home") + test["params"]
             )
-        )
-        view = AllVideosView()
-        view.setup(request)
-        view.get(request)
-        context = view.get_context_data()
-        self.assertIn("videos", context)
-        self.assertEqual(len(context["videos"]), 2)
+            view = AllVideosView.as_view()(request)
+            context = view.context_data
+            self.assertIn("videos", context)
+            self.assertEqual(context["view"].page_range, test["page_range"])
+            self.assertEqual(len(context["videos"]), test["count"])
