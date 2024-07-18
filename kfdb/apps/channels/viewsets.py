812be8156ef275f6dd6398b246d4583c3eaf5ba6
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-07-17 17:21:11 -0700

    Improve API filtering and docs

--- kfdb/apps/channels/viewsets.py
+++ kfdb/apps/channels/viewsets.py
@@ -1,15 +1,18 @@
+from drf_spectacular.utils import extend_schema
+
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from .models import Channel
 from .serializers import ChannelSerializer
 
 
+@extend_schema(
+    description=(
+        "Channels are the most basic way to filter Kinda Funny"
+        " content. The main channels are KF Games and KF Prime."
+    )
+)
 class ChannelViewSet(ReadOnlyModelViewSet):
     queryset = Channel.objects.all()
     serializer_class = ChannelSerializer
-    search_fields = (
-        "name",
-        "blurb",
-    )
-
-    filterset_fields = ("name", "slug")
+    filterset_fields = ("id", "name", "slug")
