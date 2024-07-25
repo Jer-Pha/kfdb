d119e26d5523968863556c2d381cf50e467e5b25
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-07-24 18:33:13 -0700

    Add robots.txt and humans.txt

--- kfdb/apps/core/urls.py
+++ kfdb/apps/core/urls.py
@@ -88,3 +88,19 @@ urlpatterns += [
         name="api_docs",
     ),
 ]
+
+# Text files
+urlpatterns += [
+    path(
+        "robots.txt",
+        TemplateView.as_view(
+            template_name="robots.txt", content_type="text/plain"
+        ),
+    ),
+    path(
+        "humans.txt",
+        TemplateView.as_view(
+            template_name="humans.txt", content_type="text/plain"
+        ),
+    ),
+]
