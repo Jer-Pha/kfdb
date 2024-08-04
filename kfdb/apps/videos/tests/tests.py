83c2a254a3bec13e176e0d29f40d2d51aec39dae
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-08-04 16:28:27 -0700

    Add tests for AllVideosChartsView

--- kfdb/apps/videos/tests/tests.py
+++ kfdb/apps/videos/tests/tests.py
@@ -8,6 +8,7 @@ from rest_framework.test import APIRequestFactory
 from ..models import Video
 from ..serializers import VideoSerializer
 from ..views import (
+    AllVideosChartsView,
     AllVideosView,
     VideoBlurbView,
     VideoDetailsView,
@@ -69,13 +70,15 @@ class VideoViewsTest(TestCase):
         )
         guest = Host.objects.create(name="Test Guest")
         producer = Host.objects.create(name="Test Producer", kf_crew=True)
-        show = Show.objects.create(name="Test Show")
 
-        for i in range(5):
-            video_id = get_random_string(length=11)
+        for i in range(12):
+            show = Show.objects.create(
+                name=f"test show {i}",
+            )
+            video_id = f"012345678{i}"
             video = Video.objects.create(
                 title=f"Test Video ({video_id})",
-                release_date=datetime.now().date(),
+                release_date=f"2024-{str(i+1).zfill(2)}-01",
                 show=show,
                 channel=channel,
                 producer=producer,
@@ -165,6 +168,82 @@ class VideoViewsTest(TestCase):
             context["video"],
             Video.objects.only("link", "title", "video_id").get(
                 video_id=video_id
+            ),
+        )
+
+    def test_show_charts_view(self):
+        """Tests AllVideosChartsView()."""
+        self.maxDiff = None
+        request = RequestFactory().get(reverse("videos_home"))
+        view = AllVideosChartsView.as_view()(request)
+        context = view.context_data
+        self.assertEqual(
+            context["doughnut_data"],
+            {
+                "labels": [f"test show {i}" for i in range(10)] + ["Other"],
+                "datasets": [
+                    {
+                        "label": " Shows",
+                        "data": ([1] * 10) + [2],
+                        "borderWidth": 1,
+                    },
+                ],
+            },
+        )
+        self.assertEqual(
+            context["doughnut_fallback"],
+            list(
+                zip(
+                    [f"test show {i}" for i in range(10)] + ["Other"],
+                    ([1] * 10) + [2],
+                )
+            ),
+        )
+        self.assertEqual(
+            context["bar_data"],
+            {
+                "labels": [
+                    "Jan '24",
+                    "Feb '24",
+                    "Mar '24",
+                    "Apr '24",
+                    "May '24",
+                    "Jun '24",
+                    "Jul '24",
+                    "Aug '24",
+                    "Sep '24",
+                    "Oct '24",
+                    "Nov '24",
+                    "Dec '24",
+                ],
+                "datasets": [
+                    {
+                        "label": " Video",
+                        "data": [1] * 12,
+                    },
+                ],
+            },
+        )
+        self.assertEqual(
+            context["bar_fallback"],
+            list(
+                zip(
+                    [
+                        "Jan '24",
+                        "Feb '24",
+                        "Mar '24",
+                        "Apr '24",
+                        "May '24",
+                        "Jun '24",
+                        "Jul '24",
+                        "Aug '24",
+                        "Sep '24",
+                        "Oct '24",
+                        "Nov '24",
+                        "Dec '24",
+                    ],
+                    [(1,)] * 12,
+                )
             ),
         )
 
