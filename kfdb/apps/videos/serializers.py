e73bb34f7d496d4f1872cb16410a1d514b914f58
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-08-19 14:45:21 -0700

    Fix videos title output (API)

--- kfdb/apps/videos/serializers.py
+++ kfdb/apps/videos/serializers.py
@@ -1,4 +1,6 @@
 from copy import deepcopy
+from html import unescape
+from re import sub
 
 from rest_framework_json_api import serializers
 from rest_framework_json_api.relations import ResourceRelatedField
@@ -24,6 +26,7 @@ class VideoSerializer(serializers.ModelSerializer):
     hosts = StringResourceRelatedField(queryset=Host.objects, many=True)
     producer = StringResourceRelatedField(queryset=Host.objects)
     show = StringResourceRelatedField(queryset=Show.objects)
+    title = serializers.CharField
 
     class Meta:
         model = Video
@@ -40,10 +43,19 @@ class VideoSerializer(serializers.ModelSerializer):
         ]
 
     def to_representation(self, instance):
-        """Remove falsey values from API results."""
+        """Remove falsey values from API results.
+
+        `title` is unescaped then all extra whitespace is removed.
+        """
         obj = super().to_representation(instance)
         non_null = deepcopy(obj)
         for key in obj.keys():
             if not obj[key]:
                 non_null.pop(key)
+        if "title" in non_null:
+            non_null["title"] = sub(
+                " +",
+                " ",
+                unescape(non_null["title"]).strip(),
+            )
         return non_null
